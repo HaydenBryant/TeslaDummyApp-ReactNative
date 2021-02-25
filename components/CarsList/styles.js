ddec0486875b63b7099d19fd4c3c9c7fc07532c4
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-02-25 15:18:43 -0500

    App and list slide/snap works. Having trouble with screen fit

---
+++ components/CarsList/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: Dimensions.get("screen").height,
+    // height: "100%",
+    // display: "flex",
+    flex: 1,
+  },
+});
+
+export default styles;
